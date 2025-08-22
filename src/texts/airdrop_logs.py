a468a0962b2e5db99be807a7b0178527c8df0fdc
Gleb Khyl <glebkhyl@gmail.com>
2025-08-22 12:38:59 +0300

    fix texts

--- src/texts/airdrop_logs.py
+++ src/texts/airdrop_logs.py
@@ -1,4 +1,3 @@
-# src/worker/airdrop_logs.py
 class AirdropLogs:
 
     CLICK_INVITER = (
@@ -11,38 +10,37 @@ class AirdropLogs:
 
     NEW_REG = (
         "🤝 <b>Новая регистрация</b> 🤝\n\n"
-        "↘️ <b>Пригласитель:</b> ID{inviter_id} | {inviter_email} | {inviter_plan}\n"
+        "↘️ <b>Пригласитель:</b> ID{inviter_id} | {inviter_email} | {inviter_plan}<\n"
         "↖️ <b>Приглашённый:</b> ID{invitee_id} | {invitee_email}"
     )
 
     INVITED_PERFORMACE_TERMS = (
         "✅ Условия выполнены! ✅\n\n"
-        "↘️ Пригласитель:</b> ID{inviter_id} | {inviter_email} | {inviter_plan}\n"
-        "↖️ Приглашенный:</b> ID{invitee_id} | {invitee_email}\n\n"
+        "↘️ <b>Пригласитель:</b> ID{inviter_id} | {inviter_email} | {inviter_plan}\n"
+        "↖️ <b>Приглашенный:</b> ID{invitee_id} | {invitee_email}\n\n"
         "📊 Статус: Готов к начислению токенов."
     )
 
     TOKENS_SENT = (
         "💰 Начисление токенов 💰\n\n"
         "🎁 Награда: по {amount} KOLLABIUM\n"
-        "↘️ Пригласитель:</b> ID{inviter_id} | {inviter_email} | {inviter_plan}\n"
-        "↖️ Приглашенный:</b> ID{invitee_id} | {invitee_email}\n\n"
+        "↘️ <b>Пригласитель:</b> ID{inviter_id} | {inviter_email} | {inviter_plan}\n"
+        "↖️ <b>Приглашенный:</b> ID{invitee_id} | {invitee_email}\n\n"
     )
 
     NEW_INVITER = (
         "🔰 Новый Пригласитель 🔰\n\n"
-        "👤 Пользователь:</b> ID{invitee_id} | {invitee_email}\n\n"
+        "👤 <b>Пользователь:</b> ID{invitee_id} | {invitee_email}\n\n"
         "🔰 Условия AirDrop: ✅\n"
         "🔰 Платный тариф: ✅ {invitee_plan}"
     )
 
     INVITER_CHANGE_PLAN = (
         "🔄 Смена тарифа пригласителя 🔄\n\n"
-        "👤 Пользователь:</b> ID{invitee_id} | {invitee_email\n\n"
+        "👤 <b>Пользователь:</b> ID{invitee_id} | {invitee_email}\n\n"
         "📈 Изменения:\n"
-        # "🔰 Старый тариф: {old_plan}\n"
-        "🔰 Новый тариф: {new_plan}\n\n"
-        "📊 Эффект: Бонус изменен на {amount} KOLLLABIUM"
+        "🔰 <b>Новый тариф:</b> {new_plan}\n\n"
+        "📊 <b>Эффект:</b> Бонус изменен на {amount} KOLLLABIUM"
     )
 
     # INFO: добивить остальные
